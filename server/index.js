8e46f5925b377e07074b27a96e38ded95a740c7a
Mahmud Sajjad Abeer <mahmudsajjad.abeer@gmail.com>
2022-02-03 17:06:01 +0600

    Project: Server boilerplates - Node, Express, Socket.io

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected! Socket id: ${socket.id}`);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING!");
+});
